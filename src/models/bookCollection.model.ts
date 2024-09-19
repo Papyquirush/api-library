04129e9906274bdc9b1d1d922f8193034ccc364a
p2207127 <florian.dussud@etu.univ-lyon1.fr>
2024-09-19 10:07:09 +0200

    bookcollection model

--- src/models/bookCollection.model.ts
+++ src/models/bookCollection.model.ts
@@ -1,19 +1,51 @@
-import { Model } from "sequelize";
+import { Model,DataTypes } from "sequelize";
 import sequelize from "../config/database"; // Connexion à la base de données
+import { Book } from "./book.model";
 
 export interface BookCollectionAttributes {
+  id?: number;
+  book_id: number;
+  available: boolean;
+  state : number;
+ 
 }
 
 export class BookCollection
   extends Model<BookCollectionAttributes>
   implements BookCollectionAttributes
 {
+  public id?: number;
+  public book_id!: number;
+  public available!: boolean;
+  public state!: number;
+  
 }
 
 BookCollection.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    
+  },
   {
     sequelize,
     tableName: "BookCollection",
   }
 );
+
+BookCollection.belongsTo(Book, { foreignKey: "id", as: "book_id" });
